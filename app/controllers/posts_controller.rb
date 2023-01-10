15554aafa42d4ba612307567b666ffa491070a1d
Eric Furrevig <efurrevig@gmail.com>
2023-01-09 21:41:20 -0500

    Devise views, post page, index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+    before_action :require_login, only: :new
+    skip_before_action :require_login, only: [:index, :show]
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+    end
+
+    private
+
+    def require_login
+        unless user_signed_in?
+            redirect_to posts_path, alert: "You need to be signed in to do this"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+
+  resources :posts
+  resources :users do
+    resources :posts
+  end
 end
