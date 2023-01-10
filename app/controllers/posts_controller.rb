47b95ffb7ada057d8ebe28158370fbe8423a86a3
Eric Furrevig <efurrevig@gmail.com>
2023-01-10 15:51:48 -0500

    must be signed in to view post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,34 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def show
+    
+    end
+
     def new
         @post = Post.new
     end
 
     def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        if @post.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+
     end
 
     private
 
-    def require_login
-        unless user_signed_in?
-            redirect_to posts_path, alert: "You need to be signed in to do this"
+        def require_login
+            unless user_signed_in?
+                redirect_to posts_path, alert: "You need to be signed in to do this"
+            end
         end
-    end
+
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
 end
